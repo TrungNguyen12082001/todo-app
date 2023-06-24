773c8c0bb1c06b7b45a630a4a8410b94d1719449
TrungNguyen <nguyentattrung1208@gmail.com>
2023-06-24 22:10:54 +0700

    Created Profle Component and Layout

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,5 +1,6 @@
 import { Grid } from '@mui/material';
 import { FC, ReactElement } from 'react';
+import { Profile } from '../profile/profile';
 
 export const Sidebar: FC = (): ReactElement => {
   return (
@@ -19,7 +20,7 @@ export const Sidebar: FC = (): ReactElement => {
         alignItems: 'center',
       }}
     >
-      <h2>Sidebar Area</h2>
+      <Profile />
     </Grid>
   );
 };
