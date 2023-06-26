085e0de76e813f66091c21e748e42ba4dba980f0
tnguyen667 <trungtat.nguyen@dxc.com>
2023-06-26 13:45:21 +0700

    Task Area Layout

--- src/components/taskArea/taskArea.tsx
+++ src/components/taskArea/taskArea.tsx
@@ -1,10 +1,47 @@
-import { Grid } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import { FC, ReactElement } from 'react';
+import { format } from 'date-fns';
 
 export const TaskArea: FC = (): ReactElement => {
+  /**PPPP: today's day,  month, date and year */
   return (
     <Grid item md={8} px={4}>
-      <h2>Content Area</h2>
+      <Box mb={8} px={4}>
+        <h2>
+          Status of Your Tasks As On{' '}
+          {format(new Date(), 'PPPP')}
+        </h2>
+      </Box>
+      <Grid
+        container
+        display="flex"
+        justifyContent="center"
+      >
+        <Grid
+          item
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-around"
+          alignItems="center"
+          md={10} // responsive for medium device, it'll occupy 10 columns
+          xs={12} // responsive for small device, it'll occupy 12 columns
+          mb={8}
+        >
+          <Box>Task Counter</Box>
+          <Box>Task Counter</Box>
+          <Box>Task Counter</Box>
+        </Grid>
+        <Grid
+          item
+          display="flex"
+          flexDirection="column"
+          xs={10}
+          md={8}
+        >
+          <Box>Tasks Will Come Over Here</Box>
+          <Box>Tasks Will Come Over Here</Box>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
