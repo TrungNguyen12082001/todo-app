645e50fa8355154f7b9c3c5214d23bf2a571be59
TrungNguyen <nguyentattrung1208@gmail.com>
2023-06-26 14:50:58 +0700

    Added Interface of Task Counter and Making Values Dynamic

--- src/components/taskCounter/taskCounter.tsx
+++ src/components/taskCounter/taskCounter.tsx
@@ -1,7 +1,14 @@
 import { FC, ReactElement } from 'react';
 import { Avatar, Box, Typography } from '@mui/material';
 
-export const TaskCounter: FC = (): ReactElement => {
+import { ITaskCounter } from './interfaces/ITaskCounter';
+import { Status } from '../createTaskForm/enums/Status';
+
+export const TaskCounter: FC<ITaskCounter> = (
+  props,
+): ReactElement => {
+  // Destructure props
+  const { status = Status.completed, count = 0 } = props;
   return (
     <>
       <Box
