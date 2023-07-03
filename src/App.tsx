8879b55a1573a07647a4dde65a49228307b8bd29
TrungNguyen <nguyentattrung1208@gmail.com>
2023-07-03 11:07:31 +0700

    Integrating Tanstack Query

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,25 @@ import { ThemeProvider, CssBaseline } from '@mui/material';
 import { customTheme } from './theme/customTheme';
 import { DashBoard } from './pages/dashboard/dashboard';
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
 const App: FC = (): ReactElement => {
   // This function would return a constructor function which React uses in turn to generate JSX React component
+  // set ReactQueryDevtools with initialIsOpen = false means open the React query dev tools in a minimized state.
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline />
-      <DashBoard />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <DashBoard />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
